b5f41de688598df8c8cdef5e88bdc2dfba06cab4
Zack <zack_jian@hotmail.com>
2016-07-12 16:53:14 +0800

    綠燈_第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190

--- PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
@@ -16,9 +16,29 @@ namespace PotterShoppingCart
         {
             double SumPrice = 0;
 
-            foreach (var item in orders)
+            var group = orders.GroupBy(s => new { s.Name, s.Price })
+                .Select(s => new
+                {
+                    Name = s.Key.Name,
+                    Price = s.Key.Price,
+                    Qty = s.Count()
+                }).ToList();
+
+            
+            switch(group.Count)
             {
-                SumPrice += item.Price * item.Qty;
+                case 1:
+                    foreach(var item in group)
+                    {
+                        SumPrice += item.Price * item.Qty;
+                    }
+                    break;
+                case 2:
+                    foreach(var item in group)
+                    {
+                        SumPrice += item.Price * item.Qty * 0.95;
+                    }
+                    break;
             }
 
             return SumPrice;
@@ -37,14 +57,14 @@ namespace PotterShoppingCart
 
     public class PotterEpisode
     {
-        public string Episode1 { get; set; }
+        public string Episode1 { get { return "EP1"; } }
 
-        public string Episode2 { get; set; }
+        public string Episode2 { get { return "EP2"; } }
 
-        public string Episode3 { get; set; }
+        public string Episode3 { get { return "EP3"; } }
 
-        public string Episode4 { get; set; }
+        public string Episode4 { get { return "EP4"; } }
 
-        public string Episode5 { get; set; }
+        public string Episode5 { get { return "EP5"; } }
     }
 }
