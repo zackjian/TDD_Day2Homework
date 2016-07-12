3d1a1d3dd3b12043d6274db90578c58cd6a9d09a
Zack <zack_jian@hotmail.com>
2016-07-12 16:57:20 +0800

    紅燈_一二三集各買了一本，價格應為100*3*0.9=270

--- PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -52,5 +52,32 @@ namespace PotterShoppingCart.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+
+        [TestMethod()]
+        public void Test_Fisrt_Episode_Buy_1_Second_Episode_Buy_1_Third_Episode_Buy_1_Other_Episode_Not_Buy_Price_Should_Be_100_Multiply_3_Multiply_Dot9_Equal_270()
+        {
+            var potterEpisode = new PotterEpisode();
+
+            //arrange
+            var target = new PotterShoppingCart();
+            var orders = new List<Order>
+            {
+                new Order { Name= potterEpisode.Episode1, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode2, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode3, Price=100, Qty=1 }
+            };
+
+            //act 
+            double actual = target.CalculatePrice(orders);
+
+            //assert
+            var expected = 270;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
     }
 }
