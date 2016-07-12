53abe89112adfe95d0c019d4f570bf663b7b2134
Zack <zack_jian@hotmail.com>
2016-07-12 16:11:46 +0800

    紅燈_Test_Fisrt_Episode_Buy_1_Other_Episode_Not_Buy_Price_Should_Be_100_Multiply_1_Equal_100

---
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class PotterShoppingCart
+    {
+        public PotterShoppingCart()
+        {
+
+        }
+
+        public object CalculatePrice(List<Order> orders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class Order
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Qty { get; set; }
+    }
+
+    public class PotterEpisode
+    {
+        public string Episode1 { get; set; }
+
+        public string Episode2 { get; set; }
+
+        public string Episode3 { get; set; }
+
+        public string Episode4 { get; set; }
+
+        public string Episode5 { get; set; }
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void Test_Fisrt_Episode_Buy_1_Other_Episode_Not_Buy_Price_Should_Be_100_Multiply_1_Equal_100()
+        {
+            var potterEpisode = new PotterEpisode();
+
+            //arrange
+            var target = new PotterShoppingCart();
+            var orders = new List<Order>
+            {
+                new Order { Name=potterEpisode.Episode1, Price=100, Qty=1 }
+            };
+
+            //act
+            var actual = target.CalculatePrice(orders);
+
+            //assert
+            var expected = 100;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
