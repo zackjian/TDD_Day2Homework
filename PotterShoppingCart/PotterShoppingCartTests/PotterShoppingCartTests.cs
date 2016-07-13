bc0b0e1b47736b9a23b697fa2c8d26a57142ebbd
Zack <zack_jian@hotmail.com>
2016-07-13 09:39:36 +0800

    紅燈_一二三四集各買了一本，價格應為100*4*0.8=320

--- PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -75,9 +75,32 @@ namespace PotterShoppingCart.Tests
             var expected = 270;
 
             Assert.AreEqual(expected, actual);
-
         }
 
+
+        [TestMethod()]
+        public void Test_Fisrt_Episode_Buy_1_Second_Episode_Buy_1_Third_Episode_Buy_1_Fourth_Episode_Buy_1_Other_Episode_Not_Buy_Price_Should_Be_100_Multiply_4_Multiply_Dot8_Equal_320()
+        {
+            var potterEpisode = new PotterEpisode();
+
+            //arrange
+            var target = new PotterShoppingCart();
+            var orders = new List<Order>
+            {
+                new Order { Name= potterEpisode.Episode1, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode2, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode3, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode4, Price=100, Qty=1 }
+            };
+
+            //act 
+            double actual = target.CalculatePrice(orders);
+
+            //assert
+            var expected = 320;
+
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
