bb5e7d95a23af6725314b95abd6118a79ec920fc
Zack <zack_jian@hotmail.com>
2016-07-13 09:43:06 +0800

    紅燈_一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375

--- PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -102,5 +102,31 @@ namespace PotterShoppingCart.Tests
             Assert.AreEqual(expected, actual);
         }
 
+
+        [TestMethod()]
+        public void Test_Fisrt_Episode_Buy_1_Second_Episode_Buy_1_Third_Episode_Buy_1_Fourth_Episode_Buy_1_Fifth_Episode_Buy_1_Price_Should_Be_100_Multiply_5_Multiply_Dot75_Equal_375()
+        {
+            var potterEpisode = new PotterEpisode();
+
+            //arrange
+            var target = new PotterShoppingCart();
+            var orders = new List<Order>
+            {
+                new Order { Name= potterEpisode.Episode1, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode2, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode3, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode4, Price=100, Qty=1 },
+                new Order { Name= potterEpisode.Episode5, Price=100, Qty=1 }
+            };
+
+            //act 
+            double actual = target.CalculatePrice(orders);
+
+            //assert
+            var expected = 375;
+
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
